be4be01ddc99688cebc50b11beb2ac4c35e39fa3
amarj <amarjadhav0326@gmail.com>
2023-08-12 22:20:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/javatutorial/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.javatutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView im;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        im=findViewById(R.id.logo);
+        Animation scale = AnimationUtils.loadAnimation(this, R.anim.scaleanimation);
+        im.setAnimation(scale);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(SplashActivity.this , LoginActivity.class);
+                startActivity(in);
+                finish();
+            }
+        },5000);
+
+    }
+}
