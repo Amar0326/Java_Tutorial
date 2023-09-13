f625de8de662ab2ad6aa518e741ba75b2a88d34d
amarj <amarjadhav0326@gmail.com>
2023-09-13 09:21:55 +0530

    Adding Compiler intend to home page

--- app/src/main/java/com/example/javatutorial/HomeActivity.java
+++ app/src/main/java/com/example/javatutorial/HomeActivity.java
@@ -2,13 +2,31 @@ package com.example.javatutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
+TextView Cbtn,StartBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Cbtn = findViewById(R.id.cbtn);
+        StartBtn = findViewById(R.id.startBtn);
+        StartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "Content Add Soon", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Cbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,CompilerActivity.class));
+            }
+        });
     }
 }
